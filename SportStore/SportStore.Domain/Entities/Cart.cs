b7ee97fa38aa5a6dc1ac727afbfa8401624afb7e
relentless <grantac@hotmail.com>
2011-02-21 13:18:31 +0000

    started to add cart

---
+++ SportStore/SportStore.Domain/Entities/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportStore.Domain.Entities {
+    public class Cart {
+        private List<CartLine> _lines = new List<CartLine>();
+        public IList<CartLine> Lines { get { return _lines; } }
+
+        public void  Add( Product product, int quantity ){
+            _lines.Add(new CartLine { Product = product, Quantity = quantity });
+        }
+
+        public void Clear() {
+            _lines.Clear();
+        }
+    }
+
+    public class CartLine {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- SportStore/SportStore.WebUI/Views/Products/ProductSummary.ascx
+++ SportStore/SportStore.WebUI/Views/Products/ProductSummary.ascx
@@ -1,6 +1,11 @@
 ﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<SportStore.Domain.Entities.Product>" %>
 <div class="item">
     <h3><%=Html.Encode(Model.Name) %></h3>
+    
+    <% using (Html.BeginForm("AddProduct", "Cart")) { %>
+        <%= Html.HiddenFor( x => x.ProductID ) %>
+        <input type="submit" value="Add to cart" />
+    <% } %>
     <%=Html.Encode(Model.Description)%>
     <h4><%=Html.Encode(Model.Price.ToString("c"))%></h4>
 </div>
